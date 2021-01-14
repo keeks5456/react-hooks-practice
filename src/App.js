15850d9c0e3af3cb7891e4ac42e0a5c5f5638156
keeks5456 <Akikogreen08@gmail.com>
2021-01-13 17:14:03 -0800

    use effect conditional render

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import HookCounterTwo from './components/HookCounterTwo'
 import ClassCounterTwo from './components/ClassCounterTwo';
 import HookCounter3 from './components/HookCounter3'
 import HookCounterFour from './components/HookCounterFour';
+import HookCounterFive from './components/HookCounterFive';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
 			{/* <ClassCounter /> */}
-      <HookCounter />
+      {/* <HookCounter /> */}
       {/* <HookCounterTwo /> */}
       {/* <ClassCounterTwo /> */}
       {/* <HookCounter3 /> */}
       {/* <HookCounterFour /> */}
+      <HookCounterFive />
 		</div>
 	);
 }
--- src/components/HookCounterFive.js
+++ src/components/HookCounterFive.js
@@ -1,19 +1,25 @@
 import React, {useEffect, useState} from 'react';
 
-function HookCounter() {
+function HookCounterFive() {
 
     const [count, setCount] = useState(0) //array destructuring
     const [likes, setLikes] = useState(0)
+    const [name, setName] = useState('') //now have initialized a name initial state
+
     useEffect(() => {
         console.log(document.title)
         document.title = `You have clicked ${count} times`
-    })
+    }, [count]) 
+    
+    // without this second parameter we risk the dom getting updates with name and count state updates, 
+    //with it we can specify what we are trying t update on the dom in this component
     return (
         <div>
+            <input type="text" value={name} onChange={e => setName(e.target.value)}/>
             <button onClick={() => setCount(count + 1)}> Count {count}</button>
             <button onClick={() => setLikes(likes + 1)}> Likes {likes}</button>
         </div>
     );
 }
 
-export default HookCounter;
+export default HookCounterFive;
